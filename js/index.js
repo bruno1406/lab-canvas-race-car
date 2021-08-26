64adc116b10823bb5f1bf1538b58216c3170a6ec
Bruno Barbagelata <bruno140685@gmail.com>
2021-08-26 16:35:03 -0400

    iteration 1 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,22 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    let canvas = document.getElementById("canvas");
+    canvas.style.backgroundImage = "url('/images/road.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "cover";
+
+    let imgTag = document.createElement("img");
+    console.log(imgTag);
+    imgTag.setAttribute("src", "./images/car.png");
+    imgTag.setAttribute("alt", "car image");
+    imgTag.setAttribute("height", "200px");
+    imgTag.setAttribute("width", "100px");
+
+    let parent = document.getElementById("game-board");
+    parent.appendChild(imgTag);
+  }
 };
