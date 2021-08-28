821ea0d30554bdcfd4c8a614aed615519850e83d
Bruno Barbagelata <bruno140685@gmail.com>
2021-08-28 14:12:54 -0400

    advanced with iteration 3

--- js/index.js
+++ js/index.js
@@ -1,29 +1,70 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
   };
+};
 
-  function startGame() {
-    drawRoad();
-    drawCar();
+function startGame() {
+  drawRoad();
+  car.drawCar(215, 540);
+}
+
+function drawRoad() {
+  const road = new Image();
+  road.src = "./images/road.png";
+  road.onload = function () {
+    ctx.drawImage(road, 0, 0, 500, 700);
+  };
+}
+
+class Car {
+  constructor() {
+    this.x = 215;
+    this.y = 540;
   }
 
-  function drawRoad() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-
-    canvas.style.backgroundImage = "url('./images/road.png')";
-    canvas.style.backgroundRepeat = "no-repeat";
-    canvas.style.backgroundSize = "cover";
-  }
-  function drawCar() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-
+  drawCar(x, y) {
     const car = new Image();
     car.src = "./images/car.png";
     car.onload = function () {
-      ctx.drawImage(car, 215, 560, 70, 140);
+      ctx.drawImage(car, x, y, 70, 140);
     };
   }
-};
+
+  moveLeft() {
+    this.x -= 8;
+  }
+  moveRight() {
+    this.x += 8;
+  }
+}
+
+const car = new Car();
+
+window.addEventListener("keydown", moveCar, true);
+
+function moveCar(event) {
+  switch (event.code) {
+    case "ArrowLeft":
+      // Handle "turn left"
+      car.moveLeft();
+      break;
+
+    case "ArrowRight":
+      // Handle "turn right"
+      car.moveRight();
+      break;
+  }
+
+  updateCanvas();
+}
+
+function updateCanvas() {
+  ctx.clearRect(0, 0, 500, 700);
+  car.drawCar();
+}
+
+updateCanvas();
